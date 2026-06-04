84b3ea3f3a34fe6b1689ae31684f72ef4429d71d
dimasrendyf <dimasrendy55@gmail.com>
2026-06-04 10:57:54 +0700

    add new onboarding page

---
+++ futsal.c3-12345/Onboarding.swift
@@ -0,0 +1,18 @@
+//
+//  Onboarding.swift
+//  futsal.c3-12345
+//
+//  Created by Dimas Rendy Febianto on 04/06/26.
+//
+
+import SwiftUI
+
+struct Onboarding: View {
+    var body: some View {
+        Text("Hello, This is onboarding page")
+    }
+}
+
+#Preview {
+    Onboarding()
+}
