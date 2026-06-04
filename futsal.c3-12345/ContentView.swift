05787c590ef48625f64b2a9328e9a6436aa4437b
KrisH <baguskrishnanditya@gmail.com>
2026-06-04 10:37:28 +0700

    Initial Commit

---
+++ futsal.c3-12345/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  futsal.c3-12345
+//
+//  Created by Bagus Krishna on 04/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ futsal.c3-12345/futsal_c3_12345App.swift
@@ -0,0 +1,17 @@
+//
+//  futsal_c3_12345App.swift
+//  futsal.c3-12345
+//
+//  Created by Bagus Krishna on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct futsal_c3_12345App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
