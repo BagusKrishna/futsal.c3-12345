fb08568f51dddf12080a0eb2557ebf3cb3e72c13
KrisH <baguskrishnanditya@gmail.com>
2026-06-04 11:47:18 +0700

    new page training home

---
+++ futsal.c3-12345/TrainingHome.swift
@@ -0,0 +1,14 @@
+//
+//  TrainingHome.swift
+//  futsal.c3-12345
+//
+//  Created by Bagus Krishna on 04/06/26.
+//
+
+import SwiftUI
+
+struct TrainingHome: View {
+    var body: some View {
+        Text("Ini training home bro")
+    }
+}
