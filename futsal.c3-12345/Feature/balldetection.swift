f4443c677f1bdaee26f891780ec69a4e501b28f0
KrisH <baguskrishnanditya@gmail.com>
2026-06-04 10:56:48 +0700

    ball detection new branch

---
+++ futsal.c3-12345/Feature/balldetection.swift
@@ -0,0 +1,15 @@
+//
+//  balldetection.swift
+//  futsal.c3-12345
+//
+//  Created by Bagus Krishna on 04/06/26.
+//
+
+import SwiftUI
+
+struct balldetection: View {
+    var body: some View {
+        Text("THis is BALL DETECTION")
+        Text("Every touch matter")
+    }
+}
